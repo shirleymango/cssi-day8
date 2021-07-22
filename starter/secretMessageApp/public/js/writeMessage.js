64c3b3ab5c76dbee0d9c33a2b3f60b1095ff0bca
Shirley Zhu <shirleyzhu@gmail.com>
2021-07-22 19:50:55 +0000

    writing message

--- starter/secretMessageApp/public/js/writeMessage.js
+++ starter/secretMessageApp/public/js/writeMessage.js
@@ -1,9 +1,40 @@
 const messagesRef = firebase.database().ref();
 
-messagesRef.push({
-    message: "This was made with Javascript",
-    passcode: "JavaScript",
-    third: "test"
-});
+const submitMessage = (userMessage, userPasscode) => {
+    messagesRef.push({
+    message: userMessage,
+    passcode: userPasscode
+    });
+}
 
-console.log("is running");
+document.querySelector("#sendMsg").addEventListener("click", (e) => {
+    let passcode = document.querySelector("#passcode").value;
+    let hasLowerCase = false, hasUpperCase = false, hasNumber = false, hasCharacter = false;
+    for (let i = 0; i < passcode.length; i++) {
+        let ascii = passcode.charCodeAt(i);
+        if (ascii >= 65 && ascii <= 90) {
+            hasUpperCase = true;
+            hasCharacter = true;
+        }
+        else if (ascii > 96 && ascii<123) {
+            hasLowerCase = true;
+            hasCharacter = true;
+        }
+        else if (ascii>47 && ascii<58) {
+            hasNumber = true;
+        }
+    }
+    console.log(passcode);
+    if (!(hasLowerCase && hasLowerCase && hasNumber && hasCharacter)) {
+        alert("Your passcode must contain an uppercase letter, a lowercase letter, and a number.");
+        return;
+    }
+    console.log("A".charCodeAt(0) == 65);
+    let message = document.querySelector("#message").value;
+    if (message.length > 100) {
+        alert("Messages can only be 100 characters long. Your message is too long");
+        return;
+    }
+    submitMessage(message, passcode);
+    document.querySelector("form").reset();
+});
